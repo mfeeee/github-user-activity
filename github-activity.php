8c38985d66ca4d3e565c3e9aa69c34bad11b5f5d
mafe <mfernandarocosta@gmail.com>
2026-02-20 14:25:21 -0300

    add HTTP context options for GitHub API requests

--- github-activity.php
+++ github-activity.php
@@ -1,5 +1,17 @@
 <?php
 
+$opts = [
+    'http' => [
+        'method'=>"GET",
+        // Use CRLF \r\n to separate multiple headers
+        'header' => "Accept: application/json\r\n" .
+                    "X-Github-Api-Version: 2022-11-28\r\n" .
+                    "User-Agent: Github-User-Activity\r\n"
+    ]
+];
+
+$context = stream_context_create($opts);
+
 if ($argc < 2) {
     echo "GitHub User Activity Guide\n" . PHP_EOL;
     echo "Usage: php github-activity.php <username>" . PHP_EOL;
@@ -8,3 +20,6 @@ if ($argc < 2) {
 
 $username = $argv[1];
 $url = "https://api.github.com/users/{$username}/events";
+
+// Open the file using HTTP headers above
+$jsonData = file_get_contents($url, false, $context);
